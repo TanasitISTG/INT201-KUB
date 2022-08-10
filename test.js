2cc20fb2b2ff4f8897fae935758c2eebdf9f2d6c
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-08-10 15:31:28 +0700

    following professor

--- test.js
+++ test.js
@@ -1,56 +1,62 @@
 function add(a, b) {
     return a + b;
-  }
-  
-  function subtract(a, b) {
-      return a - b;
-  }
-  
-  function multiply(a, b) {
-      return a * b;
-  }
-  
-  function divide(a, b) {
-      return a / b;
-  }
-  
-  function operate(a, b, operator) {
-      return operator(a, b);
-  }
-  
-  let addResult = operate(1, 2, add);
-  let subtractResult = operate(1, 2, subtract);
-  let multiplyResult = operate(1, 2, multiply);
-  let divideResult = operate(1, 2, divide);
-  
-  console.log(addResult);
-  console.log(subtractResult);
-  console.log(multiplyResult);
-  console.log(divideResult);
-  
-  for (let i = 0; i < 5; i++) {
-      console.log(`This is loop ${i}`);
-  }
-  
-  
-  
-  // This could've been done better but yeah... just messin around with it
-  function sumAllBetween(num) {
-      let sum = 0;
-      for (let i = 1; i <= num; i++) {
-          sum += i;
-          array.push(i);
-      }
-      return sum;
-  }
-  
-  function findNum(num) {
-      let array = [];
-      for (let i = 1; i <= num; i++) {
-          array.push(i);
-      }
-      return array;
-  }
-  
-  let array = findNum(10);
-  console.log(array.join(' + ') + ' = ' + sumAllBetween(10));
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operator) {
+    return operator(a, b);
+}
+
+let addResult = operate(1, 2, add);
+let subtractResult = operate(1, 2, subtract);
+let multiplyResult = operate(1, 2, multiply);
+let divideResult = operate(1, 2, divide);
+
+console.log(addResult);
+console.log(subtractResult);
+console.log(multiplyResult);
+console.log(divideResult);
+
+for (let i = 0; i < 5; i++) {
+    console.log(`This is loop ${i}`);
+}
+
+// This could've been done better but yeah... just messin around with it
+function sumAllBetween(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+        array.push(i);
+    }
+    return sum;
+}
+
+function findNum(num) {
+    let array = [];
+    for (let i = 1; i <= num; i++) {
+        array.push(i);
+    }
+    return array;
+}
+
+let array = findNum(10);
+console.log(array.join(" + ") + " = " + sumAllBetween(10));
+
+function displayMessage(message) {
+    console.log("Starting message....");
+    console.log(message);
+    console.log("Ending message....");
+}
+let message = "Hello World";
+displayMessage(message);
